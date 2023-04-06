277e5023d09596d79dad6d7c82b7afdb4388f085
Shannon Bertucci <shannon.bertucci@gmail.com>
2023-04-06 00:13:18 -0700

    Better meta title

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,11 +1,12 @@
 class HomepageController < ApplicationController
     def index
-        scores = SmsGameScore.all.group_by(&:user_id).map{|user_id, records| {
-        userId: user_id, 
-        userName: records.first.user.name,
-        totalScore: records.sum(&:score)
+        scores = SmsGameScore.all.order("score DESC").group_by(&:user_id).map{|user_id, records| {
+          userId: user_id, 
+          userName: records.first.user.name,
+          totalScore: records.sum(&:score)
         }}
-    
+
         @display_scores = scores.map { |score| "| #{score[:userName]} #{score[:totalScore]} |" }.join("")
+        @display_rank = "🥇 #{scores[0][:userName]} | 🥈 #{scores[1][:userName]} | 🥉 #{scores[2]&[:userName]}"
     end
 end
